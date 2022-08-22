b30ce3efbdc577da38c7aff292c98ef8d876d7d7
Roopesh Acharya <rupeshacharya2014@gmail.com>
2022-08-23 02:44:21 +0545

    added front end with simple django functions and urls with template rendering

--- gms/urls.py
+++ gms/urls.py
@@ -15,14 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import base
-from .views import abc
-from .views import form
+
+from .views import customer_table , product, add_product , vendor , add_vendor, service, sales_list, add_service, login , index, add_customer, buy_product, sell_product, sell_services
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', abc),
-    path('form/',form,name='form'),
-    path('base/',base,name='base')
+    path('login', login, name="login"),
+    path('', index, name="index"),
+    path('add_customer',add_customer,name='add_customer'),
+    path('customer_table',customer_table,name='customer_table'),
+    path('product',product,name='product'),
+    path('add_product',add_product,name='add_product'),
+    path('vendor',vendor,name='vendor'),
+    path('add_vendor',add_vendor,name='add_vendor'),
+    path('service',service,name='service'),
+    path('add_service',add_service,name='add_service'),
+    path('buy_product',buy_product,name='buy_product'),
+    path('sell_product',sell_product,name='sell_product'),
+    path('sell_services',sell_services,name='sell_services'),
+    path('sales_list',sales_list,name='sales_list'),
+    
+
+    
 ]
--- gms/views.py
+++ gms/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render
 
+def login(request):
+    return render(request, 'login.html')
 
-def abc(request):
+def index(request):
     return render(request,"index.html")
 
+def add_customer(request):
+    return render(request,"add_customer.html")
 
-def base(request):
-    return render(request,"base.html")
+def customer_table(request):
+    return render(request, 'customer_table.html')   
 
-def form(request):
-    return render(request,"formss.html")
+def product(request):
+    return render(request, 'product.html')
 
+def add_product(request):
+    return render(request, 'add_product.html')
+
+def vendor(request):
+    return render(request, 'vendor.html')
+
+def add_vendor(request):
+    return render(request, 'add_vendor.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def add_service(request):
+    return render(request, 'add_service.html')
+
+def buy_product(request):
+    return render(request, 'buy_product.html')
+
+def sell_product(request):
+    return render(request, 'sell_product.html')
+
+def sell_services(request):
+    return render(request, 'sell_services.html')
+
+def sales_list(request):
+    return render(request, 'sales_list.html')
